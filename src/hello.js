3e7646f9cbcf660a9bef4312e9f353fbbca14008
stefaniazocco <stefaniazocco93@gmail.com>
2022-09-29 18:57:24 +0200

    add message

--- src/hello.js
+++ src/hello.js
@@ -1,10 +1,11 @@
 import React from "react"
+import Message from "./message"
 
 export class Hello extends React.Component{
     render() {
         return  <>
-                <h1>Hello, World!</h1>
-                <p>What a beautiful day!</p>
+                    <h1>Hello, World!</h1>
+                    <Message />
                 </>
     }
 }
