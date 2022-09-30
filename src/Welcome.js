4ec23b86c2216016985787f297c31864b788b269
stefaniazocco <stefaniazocco93@gmail.com>
2022-09-30 18:08:25 +0200

    add age component

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,14 +1,13 @@
 import React from "react";
-
+import { Age } from "./Age";
 
 export class Welcome extends React.Component {
     render(){
         return (
         <>
             <p>Welcome, {this.props.name}!</p>
-            <p>Your age is {this.props.age}</p>
+            <Age age={29}/>
         </>
         )
     }
 }
-
