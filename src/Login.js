5b98744d6feb17b5011bd122f58bd03faeb80cb1
stefaniazocco <stefaniazocco93@gmail.com>
2022-10-08 15:56:32 +0200

    add button

--- src/Login.js
+++ src/Login.js
@@ -5,7 +5,8 @@ export class Login extends React.Component{
     state= {
         username: "",
         password: "",
-        remember: false
+        remember: false,
+        disabled : true
     }
     handleInputChange = (event) => {
         const value = event.target.value
@@ -16,11 +17,21 @@ export class Login extends React.Component{
             [name] : type === "checkbox" ? checked : value
         })
     }
+    
+    onLogin = (event) => {
+        console.log(event)
+        const verify = event.target.disabled
+        this.setState({
+            disabled : verify
+        })
+        console.log(verify)
+    }
     render() {
         return  <div>
                     <input name="username" value={this.state.username} onChange={this.handleInputChange} />
                     <input name="password" type="password" value={this.state.password} onChange={this.handleInputChange} />
                     <input name="remember" type="checkbox" checked={this.state.remember} onChange={this.handleInputChange} />
+                    <button disabled={!this.state.username || !this.state.password} onClick={this.onLogin}>Login</button>
                 </div>
     }
 }
