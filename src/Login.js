f720ad9820317b938a9db5b0456cc36fc33c87a0
stefaniazocco <stefaniazocco93@gmail.com>
2022-10-08 16:06:55 +0200

    add reset

--- src/Login.js
+++ src/Login.js
@@ -26,12 +26,22 @@ export class Login extends React.Component{
         })
         console.log(verify)
     }
+
+    onReset = () => {
+        this.setState({
+            username: "",
+            password: "",
+            remember: false
+        })
+    }
+
     render() {
         return  <div>
                     <input name="username" value={this.state.username} onChange={this.handleInputChange} />
                     <input name="password" type="password" value={this.state.password} onChange={this.handleInputChange} />
                     <input name="remember" type="checkbox" checked={this.state.remember} onChange={this.handleInputChange} />
                     <button disabled={!this.state.username || !this.state.password} onClick={this.onLogin}>Login</button>
+                    <button onClick={this.onReset}>Reset</button>
                 </div>
     }
 }
