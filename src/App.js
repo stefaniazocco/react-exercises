8915bfc59c3aa7b4684118f02a095dac8c76e724
stefaniazocco <stefaniazocco93@gmail.com>
2022-10-10 20:03:50 +0200

    add container

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react"
 import { Login } from "./Login"
+import { Container } from "./Container"
 
 
 export class App extends React.Component{
     render() {
         return  <div>
                     <Login />
+                    <Container>
+
+                    </Container>
                 </div>
     }
 }
