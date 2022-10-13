036fa28e0b155f425d5acc32216c13d396eee58f
stefaniazocco <stefaniazocco93@gmail.com>
2022-10-13 21:45:06 +0200

    add display language

--- src/DisplayLanguage.js
+++ src/DisplayLanguage.js
@@ -1,5 +1,22 @@
 import { LanguageContext } from "./LanguageContext"
 
+const Strings = {
+    en: {
+        HELLO : "Hello, this is the English verision"
+    },
+    it: {
+        HELLO : "Ciao, questa è la versione italiana"
+    }
+}
+
 export function DisplayLanguage(){
-    
+    return (
+        <LanguageContext.Consumer>
+            {language =>{
+                return (
+                    <h1>{Strings[language].HELLO}</h1>
+                )
+            }}
+        </LanguageContext.Consumer>
+    )
 }
