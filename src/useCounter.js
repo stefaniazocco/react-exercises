9739a5b5d2ba7adc808f394463ee59654d5653c1
stefaniazocco <stefaniazocco93@gmail.com>
2022-10-20 13:54:09 +0200

    add reset

--- src/useCounter.js
+++ src/useCounter.js
@@ -10,22 +10,28 @@ function useCounter(){
         setCounter((c) => c - 1)
     }
 
+    function resetButton() {
+        setCounter(0)
+    }
+
     return {
         counter: counter,
         onIncrement: handleIncrement,
         onDecrement: handleDecrement,
+        onReset: resetButton
     }
 }
 
 export function CustomCounter(){
 
-    const {counter, onIncrement, onDecrement} = useCounter()
+    const {counter, onIncrement, onDecrement, onReset} = useCounter()
 
     return (
         <div style={{display: "flex", alignItems: "center"}}>
             <button onClick={onDecrement} style={{border: "2px solid black", height:"41px", width: "35px", fontSize: "30px"}} >-</button>
             <h1 style={{border: "2px solid black", width: "41px", textAlign: "center"}}>{counter}</h1>
             <button onClick={onIncrement} style={{border: "2px solid black", height:"41px", width: "35px", fontSize: "30px"}}>+</button>
+            <button onClick={onReset} style={{border: "2px solid black", height:"41px", fontSize: "30px"}}>RESET</button>
         </div>
     )
 }
